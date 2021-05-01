8d349ef337e971c1f5a4c7ee971efdc2ab284617
trunghieu-99 <trunghieu050999@gmail.com>
2021-05-01 10:27:50 +0700

    20210501

---
+++ crop.m
@@ -0,0 +1,9 @@
+% Crop image from 128*128 to 28*28
+% Input:    - path: path of image input
+% Output: new image
+% Date modified: 2021/05/01
+% 
+
+function x = crop(path)
+
+end
---
+++ spk_gen.m
@@ -0,0 +1,11 @@
+% Generate spike for SNN
+% Input:    - path: path of image 
+%           - N: number of bit axon destination (parameter of architecture)
+% Output: spike
+% Date modified: 2021/05/01
+% 
+
+function x = spk_gen(path, N)
+    img = imread(path);
+    
+end
